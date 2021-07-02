898d9ca7be9f9adeec2a28256a5fd1f09b46247b
Shane James <shanejames@Shanes-iMac.local>
2021-07-01 21:53:53 -0400

    add gh pages dependencies

--- src/components/myresume/resumeAction.js
+++ src/components/myresume/resumeAction.js
@@ -3,7 +3,7 @@ export default function ResumeAction() {
     <div className="h-16 border-t border-gray-primary mt-12 pt-4">
       <div className="grid grid-cols-1">
         <div className="col-span-1 flex justify-center">
-          <embed src="images/shanejamesresume.pdf" width="600px" height="600px" />
+          <embed src="images/shanejamesresume.pdf" width="500px" height="500px" />
         </div>
       </div>
     </div>
